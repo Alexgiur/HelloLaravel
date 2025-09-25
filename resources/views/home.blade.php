4a09214c09d1a3e77b1f504cf77832ca5bc0a678
Alexgiur <alxntm@gmail.com>
2025-09-25 15:40:28 +0200

    Ajout vue home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+
+    <body>
+        <h1>Bienvenue dans le HOME !</h1>
+
+        <p>{{ $greeting }} {{ $nom_utilisateur }}!</p>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+
+    $greeting = "Yo ";
+
+    $username = "Bob";
+
+    return view('home', [
+        'nom_utilisateur' => $username,
+        'greeting' => $greeting
+    ]);
+});
